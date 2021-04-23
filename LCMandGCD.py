a86cba0db1439af966124a25b0e31189fc202ed8
sandesh-prabhu <77835063+sandesh-prabhu@users.noreply.github.com>
2021-04-23 12:10:18 +0530

    Update LCMandGCD.py

--- LCMandGCD.py
+++ LCMandGCD.py
@@ -1,14 +1,11 @@
-while 1:    
-    def gcd(a,b):
-        if a==0:
-            return b
-        else:
-            return gcd(b%a,a)
-    def lcm(a,b):
-        return (a/gcd(a,b))*b
-    a=int(input("Enter the value of 'A' (Enter nothing to exit):"))
-    if a=="":
-        break
-    b=int(input("Enter the value of 'B':"))
-    print("LCM of",a,"and",b,"is",lcm(a,b))
-    print("GCD(HCF) of",a,"and",b,"is",gcd(a,b),"\n")
+def gcd(a,b):
+    if a==0:
+        return b
+    else:
+        return gcd(b%a,a)
+def lcm(a,b):
+    return (a/gcd(a,b))*b
+a=int(input("Enter the value of 'A':"))
+b=int(input("Enter the value of 'B':"))
+print("LCM of",a,"and",b,"is",lcm(a,b))
+print("GCD(HCF) of",a,"and",b,"is",gcd(a,b),"\n")
